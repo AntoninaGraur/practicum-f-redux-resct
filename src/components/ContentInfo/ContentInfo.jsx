5b2c4a65fdd7f9715a70d39cced8b28aea4ffb28
Antonina Graur <124353835+AntoninaGraur@users.noreply.github.com>
2023-07-31 22:42:18 +0200

    updating error

--- src/components/ContentInfo/ContentInfo.jsx
+++ src/components/ContentInfo/ContentInfo.jsx
@@ -5,48 +5,65 @@ import { getNews } from 'services/getNews';
 class ContentInfo extends Component {
   state = {
     news: null,
-    isLoading: false,
-      error: '',
-    status:'idle'
+    // isLoading: false,
+    error: '',
+    status: 'idle',
   };
   componentDidUpdate(prevProps, prevState) {
     if (prevProps.searchText !== this.props.searchText) {
-      this.setState({ isLoading: true });
+      this.setState({ status: 'pending' });
       getNews(this.props.searchText)
         .then(response => response.json())
         .then(data => {
-          if (data.status === 'ok') this.setState({ news: data.articles, status: 'resolved'});
+          if (data.status === 'ok')
+            this.setState({ news: data.articles, status: 'resolved' });
           else return Promise.reject(data.message);
         })
-          .catch(error => {
-            console.log('error', error)
+        .catch(error => {
+          console.log('error', error);
           this.setState({ error, status: 'rejected' });
-        })
-        .finally(() => {
-          this.setState({ isLoading: false });
         });
+      // .finally(() => {
+      //   this.setState({ isLoading: false });
+      // });
     }
   }
 
   render() {
-      const { news, isLoading, error } = this.state;
-      if (this.state.status === 'rejected') return <ErrorCard>{this.state.error}</ErrorCard>
-      else if(this.state.status === "resolved") return 
-    return (
-        <>{error && <ErrorCard>{ this.state.error}</ErrorCard>}
-        {isLoading && (
-          <div className="spinner-grow" role="status">
-            <span className="visually-hidden">Loading...</span>
-          </div>
-        )}
+    const { news, error } = this.state;
+    if (this.state.status === 'pending')
+      return (
+        <div className="spinner-grow" role="status">
+          <span className="visually-hidden">Loading...</span>
+        </div>
+      );
+    else if (this.state.status === 'resolved')
+      return (
         <ul>
-          {news &&
-            news.map(el => {
-              return <li key={el.url}> {el.title}</li>;
-            })}
+          {news.map(el => {
+            return <li key={el.url}> {el.title}</li>;
+          })}
         </ul>
-     </>
-    );
+      );
+    else if (this.state.status === 'rejected')
+      return <ErrorCard>{error}</ErrorCard>;
+
+    // return (
+    //   <>
+    //     {error && <ErrorCard>{this.state.error}</ErrorCard>}
+    //     {isLoading && (
+    //       <div className="spinner-grow" role="status">
+    //         <span className="visually-hidden">Loading...</span>
+    //       </div>
+    //     )}
+    //     <ul>
+    //       {news &&
+    //         news.map(el => {
+    //           return <li key={el.url}> {el.title}</li>;
+    //         })}
+    //     </ul>
+    //   </>
+    // );
   }
 }
 
