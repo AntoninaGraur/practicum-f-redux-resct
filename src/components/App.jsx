a258b1e86d0736ce9ffc27b6462f3c90659f5cf3
Antonina Graur <124353835+AntoninaGraur@users.noreply.github.com>
2023-08-02 13:31:04 +0200

    starting change on hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,60 +1,99 @@
 import Header from './Header/header';
 
-// import ToDoList from './ToDoList/toDoList';
-// import Modal from './Modal/modal';
-// import LoginForm from './LoginForm/LoginForm';
+// eslint-disable-next-line no-unused-vars
+import ToDoList from './ToDoList/toDoList';
+import Modal from './Modal/modal';
+import LoginForm from './LoginForm/LoginForm';
 import { nanoid } from 'nanoid';
 // import Counter from './Counter/counter'
-import { Component } from 'react';
+import { useState } from 'react';
 import Search from './Search/Search';
 import ContentInfo from './ContentInfo/ContentInfo';
 
+import React from 'react';
 
-class App extends Component {
-  state = {
-    isShowModal: false,
-    searchText: '',
-  };
-  
-  showModal = () => {
-    this.setState({ isShowModal: true });
-  };
+const App = () => {
+  const [isShowModal, setIsShowModal] = useState(false);
+  const [searchText, setSearchText] = useState('');
 
-  closeModal = () => {
-    this.setState({ isShowModal: false });
-  };
-
-  createUser = data => {
+  const showModal = () => setIsShowModal(true);
+  const closeModal = () => setIsShowModal(false);
+  function createUser(data) {
+    
     const newUser = {
       ...data,
       id: nanoid(),
-    };
-  return newUser;
+    }
+    console.log('newUser:>>', newUser);
+  }
+
+  const handleSearch = searchText => {
+    setSearchText(searchText)
   };
 
-  handleSearch = (searchText) => {
-    this.setState({searchText})
-  }
-
-  render() {
-    return (
-      <div className="container">
-        <Header showModal={this.showModal} />
-        <Search handleSearch={ this.handleSearch} />
-        <ContentInfo searchText={this.state.searchText} />
-        {/* <Counter /> */}
-        {/* <ToDoList />
-        {this.state.isShowModal && (
-          <Modal closeModal={this.closeModal}>
-            <LoginForm
-              createUser={this.createUser}
-              closeModal={this.closeModal}
-            />
-          </Modal>
-        )} */}
-      </div>
-    );
-  }
-}
+  return (
+    <div className="container">
+      <Header showModal={showModal} />
+      <Search handleSearch={handleSearch} />
+      <ContentInfo searchText={searchText} />
+      {/* <Counter /> */}
+      {/* <ToDoList /> */}
+      {isShowModal && (
+        <Modal closeModal={closeModal}>
+          <LoginForm createUser={createUser} closeModal={closeModal} />
+        </Modal>
+      )}
+    </div>
+  );
+};
 
 export default App;
+
+// class App extends Component {
+//   state = {
+//     isShowModal: false,
+//     searchText: '',
+//   };
+
+//   showModal = () => {
+//     this.setState({ isShowModal: true });
+//   };
+
+//   closeModal = () => {
+//     this.setState({ isShowModal: false });
+//   };
+
+//   createUser = data => {
+//     const newUser = {
+//       ...data,
+//       id: nanoid(),
+//     };
+//   return newUser;
+//   };
+
+//   handleSearch = (searchText) => {
+//     this.setState({searchText})
+//   }
+
+//   render() {
+//     return (
+//       <div className="container">
+//         <Header showModal={this.showModal} />
+//         <Search handleSearch={ this.handleSearch} />
+//         <ContentInfo searchText={this.state.searchText} />
+//         {/* <Counter /> */}
+//         {/* <ToDoList />
+//         {this.state.isShowModal && (
+//           <Modal closeModal={this.closeModal}>
+//             <LoginForm
+//               createUser={this.createUser}
+//               closeModal={this.closeModal}
+//             />
+//           </Modal>
+//         )} */}
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
--- src/components/Modal/modal.jsx
+++ src/components/Modal/modal.jsx
@@ -1,40 +1,53 @@
-import { Component } from 'react';
+// import { Component } from 'react';
 import { Backdrop, ModalWindow, ModalButton } from './modal.styled';
 
-class Modal extends Component {
-  state = {};
+import React from 'react';
 
-  componentDidMount() {
-    window.addEventListener('keydown', this.handlePressESC);
-  }
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.handlePressESC);
-  }
-  handlePressESC = e => {
-    if (e.code === 'Escape') this.props.closeModal();
+const Modal = ({ closeModal }) => {
+  const handlePressESC = e => {
+    console.log('object', Date.now());
+    if (e.code === 'Escape') closeModal();
   };
-  render() {
-    const { closeModal, children } = this.props;
-    return (
-      <Backdrop>
-        <ModalWindow>
-          <div className="modal-content">
-            <div className="modal-header">
-              <h2 className="modal-title"> Modal</h2>
-              <ModalButton
-                type="button"
-                aria-label="Close"
-                onClick={closeModal}
-              >
-                Close
-              </ModalButton>
-            </div>
-            <div className="modal-body">{children}</div>
-          </div>
-        </ModalWindow>
-      </Backdrop>
-    );
-  }
-}
+
+  return <div>Modal</div>;
+};
 
 export default Modal;
+
+// class Modal extends Component {
+//   state = {};
+
+//   componentDidMount() {
+//     window.addEventListener('keydown', this.handlePressESC);
+//   }
+//   componentWillUnmount() {
+//     window.removeEventListener('keydown', this.handlePressESC);
+//   }
+//   handlePressESC = e => {
+//     if (e.code === 'Escape') this.props.closeModal();
+//   };
+//   render() {
+//     const { closeModal, children } = this.props;
+//     return (
+//       <Backdrop>
+//         <ModalWindow>
+//           <div className="modal-content">
+//             <div className="modal-header">
+//               <h2 className="modal-title"> Modal</h2>
+//               <ModalButton
+//                 type="button"
+//                 aria-label="Close"
+//                 onClick={closeModal}
+//               >
+//                 Close
+//               </ModalButton>
+//             </div>
+//             <div className="modal-body">{children}</div>
+//           </div>
+//         </ModalWindow>
+//       </Backdrop>
+//     );
+//   }
+// }
+
+// export default Modal;
